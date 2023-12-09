52681595f41e87fa19e1043bda579252edbe84d5
git1worker <vladim-b@mail.ru>
2023-12-09 18:36:55 +0300

    Updated

--- s21_decimal.c
+++ s21_decimal.c
@@ -1,7 +1,33 @@
 #include "s21_decimal.h"
 
+#include <stdint.h>
+
+int getBit(s21_decimal value, int num) {
+
+} 
+
+void setBit(s21_decimal value, int num, int bitValue) {
+
+}
+
+int getExp(s21_decimal value) {
+
+}
+
+void setExp(s21_decimal value, int exp) {
+
+}
+
+int getSign(s21_decimal value) {
+
+}
+
+void setSign(s21_decimal value, int sign) {
+  
+}
+
 int s21_add(s21_decimal value_1, s21_decimal value_2, s21_decimal *result)
 {
-    
-    return 0;
+
+  return 0;
 }
--- s21_decimal.h
+++ s21_decimal.h
@@ -2,12 +2,12 @@
 #define S21_DECIMAL_H
 
 
-/* [sign][]
- * [1][]  */
+
 typedef struct 
 {
-    int bits[4];
+  unsigned bits[4];
 } s21_decimal;
+
 
 int s21_add(s21_decimal value_1, s21_decimal value_2, s21_decimal *result);
 int s21_sub(s21_decimal value_1, s21_decimal value_2, s21_decimal *result);
